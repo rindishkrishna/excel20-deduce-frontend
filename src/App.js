7a1762e8bb0fbdf9cb0383dc910f913e113132c8
AFNAN NAVAZ <afnan@pop-os.localdomain>
2020-03-26 18:55:38 +0530

    updated auth0 setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import './App.scss';
 import { Router, Route, Switch } from "react-router-dom";
-import PrivateRoute from './auth0/PrivateRoute';
 import history from './utils/history';
-import {useAuth0} from './auth0/auth0-context';
+import {login, logout, handleAuthentication, isAuthenticated} from './auth0/auth0';
 
 function App() {
-  const {isAuthenticated, loginWithRedirect, logout, loading} = useAuth0();
-  if (loading) {
-    return <div>loading ...</div>;
-  }
   return (
       <Router history={history}>
         <div>
-        {!isAuthenticated && (
-          <button onClick={() => loginWithRedirect({})}>Log in</button>
-        )}
-        {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+          { isAuthenticated() && <button onClick = {() => logout()}>logout</button> }
+          { !isAuthenticated() && <button onClick = {() => login()}>login</button> }
         </div>
         <Switch>
-          <Route path="/" exact />
-          <PrivateRoute path="/profile"  />
+          <Route path="/login" render={() => login()} />
+          <Route path="/login/callback" render={() => handleAuthentication()} />
         </Switch>
       </Router>
       
---
+++ src/auth0/http.js
@@ -0,0 +1,16 @@
+export const post = (url, data) => {
+    return fetch(url, {
+        method: 'POST',
+        body: data,
+    })
+        .then(res => res.json())
+        .catch(err => err);
+};
+
+export const get = url => {
+    return fetch(url, {
+        credentials: 'include',
+    })
+        .then(res => res.json())
+        .catch(err => console.log(err));
+};
