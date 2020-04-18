17d243453142d2f419f87205ae7d97ae6eab8694
Aswin M Prabhu <aswinmprabhu@gmail.com>
2020-04-18 23:36:46 +0530

    Use auth0 details for chat creds

--- src/components/chat/src/App.js
+++ src/components/chat/src/App.js
@@ -16,10 +16,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
- const Chat = () => {
-  const [nickname, setNickname] = useState("");
-  const [email, setEmail] = useState("");
-  const [joined, setJoined] = useState(false);
+const Chat = ({ name, email }) => {
   const [msg, setMsg] = useState("");
   const [messages, setMessages] = useState([]);
   const [scrolled, setScrolled] = useState(false);
@@ -28,182 +25,140 @@ const db = firebase.database();
   const messagesEndRef = useRef(null);
   const messagesDivRef = useRef(null);
 
-     function scrolledToBottom(el) {
-         return el.scrollHeight - el.scrollTop - el.clientHeight == 0;
-     }
-     const scrollToBottom = () => {
-         if (messagesEndRef.current && !scrolled) {
-             messagesEndRef.current.scrollIntoView();
-         }
-     };
-     useEffect(scrollToBottom, [messages]);
-     useEffect(scrollToBottom, []);
-     useEffect(() => {
-         const handleNewMessages = (data) => {
-             if (data.val()) {
-                 const size = parseInt(window.localStorage.getItem("size"));
-                 const msgs = JSON.parse(window.localStorage.getItem("messages")) || [];
-                 if (size < 200) {
-                     window.localStorage.setItem("size", JSON.stringify(size + 1));
-                     window.localStorage.setItem("last", data.val().timestamp);
-                     window.localStorage.setItem(
-                         "messages",
-                         JSON.stringify([...msgs, data.val()])
-                     );
-                     setMessages((old) => {
-                         return [...msgs, data.val()];
-                     });
-                 } else {
-                     window.localStorage.setItem("last", data.val().timestamp);
-                     const newMessages = msgs.slice(msgs.length / 2);
-                     window.localStorage.setItem(
-                         "size",
-                         JSON.stringify(newMessages.length + 1)
-                     );
-                     window.localStorage.setItem(
-                         "messages",
-                         JSON.stringify([...newMessages, data.val()])
-                     );
-                     setMessages((old) => {
-                         return [...newMessages, data.val()];
-                     });
-                 }
-             }
-         };
-         const size = window.localStorage.getItem("size");
-         if (size == 0 || size == null) {
-             chatRoom.endAt().limitToLast(100).on("child_added", handleNewMessages);
-         } else {
-             setMessages(JSON.parse(window.localStorage.getItem("messages")));
-             chatRoom
-                 .orderByChild("timestamp")
-                 .startAt(parseInt(window.localStorage.getItem("last")))
-                 .on("child_added", handleNewMessages);
-         }
-         return () => {
-             chatRoom.off("child_added", handleNewMessages);
-         };
-         // eslint-disable-next-line
-     }, []);
+  function scrolledToBottom(el) {
+    return el.scrollHeight - el.scrollTop - el.clientHeight == 0;
+  }
+  const scrollToBottom = () => {
+    if (messagesEndRef.current && !scrolled) {
+      messagesEndRef.current.scrollIntoView();
+    }
+  };
+  useEffect(scrollToBottom, [messages]);
+  useEffect(scrollToBottom, []);
+  useEffect(() => {
+    const handleNewMessages = (data) => {
+      if (data.val()) {
+        const size = parseInt(window.localStorage.getItem("size"));
+        const msgs = JSON.parse(window.localStorage.getItem("messages")) || [];
+        if (size < 200) {
+          window.localStorage.setItem("size", JSON.stringify(size + 1));
+          window.localStorage.setItem("last", data.val().timestamp);
+          window.localStorage.setItem(
+            "messages",
+            JSON.stringify([...msgs, data.val()])
+          );
+          setMessages((old) => {
+            return [...msgs, data.val()];
+          });
+        } else {
+          window.localStorage.setItem("last", data.val().timestamp);
+          const newMessages = msgs.slice(msgs.length / 2);
+          window.localStorage.setItem(
+            "size",
+            JSON.stringify(newMessages.length + 1)
+          );
+          window.localStorage.setItem(
+            "messages",
+            JSON.stringify([...newMessages, data.val()])
+          );
+          setMessages((old) => {
+            return [...newMessages, data.val()];
+          });
+        }
+      }
+    };
+    const size = window.localStorage.getItem("size");
+    if (size == 0 || size == null) {
+      chatRoom.endAt().limitToLast(100).on("child_added", handleNewMessages);
+    } else {
+      setMessages(JSON.parse(window.localStorage.getItem("messages")));
+      chatRoom
+        .orderByChild("timestamp")
+        .startAt(parseInt(window.localStorage.getItem("last")))
+        .on("child_added", handleNewMessages);
+    }
+    return () => {
+      chatRoom.off("child_added", handleNewMessages);
+    };
+    // eslint-disable-next-line
+  }, []);
 
-     const handleNameChange = (e) => setNickname(e.target.value);
-     const handleEmailChange = (e) => setEmail(e.target.value);
-     const handleClick = (e) => {
-         db.ref().child("nicknames").push({
-             nickname,
-             email,
-         });
-         setJoined(true);
-     };
-
-     const handleMsgChange = (e) => setMsg(e.target.value);
-     const handleKeyDown = (e) => {
-         if (e.key === "Enter") {
-             chatRoom.push({
-                 sender: nickname,
-                 msg,
-                 timestamp: Date.now(),
-             });
-             setMsg("");
-         }
-     };
+  const handleMsgChange = (e) => setMsg(e.target.value);
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      chatRoom.push({
+        sender: name,
+        email: email,
+        msg,
+        timestamp: Date.now(),
+      });
+      setMsg("");
+    }
+  };
 
   return (
     <div className="chatApp">
-      {!joined ? (
-        <div className="joinForm">
-          <input
-            placeholder="Nickname"
-            value={nickname}
-            onChange={handleNameChange}
-          />
-          <br />
-          <input
-            placeholder="Email"
-            value={email}
-            onChange={handleEmailChange}
-          />
-          <br />
-          <button onClick={handleClick}>Join</button>
+      <div className="chat">
+        <div
+          className="messages"
+          ref={messagesDivRef}
+          onWheel={(e) => {
+            console.log(scrolledToBottom(messagesDivRef.current));
+            if (scrolledToBottom(messagesDivRef.current)) {
+              setScrolled(false);
+            } else {
+              setScrolled(true);
+            }
+          }}
+          onTouchMove={(e) => {
+            // if (elementInViewport(messagesEndRef.current)) {
+            //   setScrolled(false);
+            // } else {
+            //   setScrolled(true);
+            // }
+          }}
+        >
+          {messages.map((message) => {
+            // console.log(message);
+            if (message["email"] === email)
+              return (
+                <div className="my-message">
+                  {/*<span id="me">{message["sender"]} :</span>*/}
+                  {/*<br />*/}
+                  <div className={"msg-text"}>{message["msg"]}</div>
+                </div>
+              );
+            else
+              return (
+                <div className="message">
+                  <span id="sender">{message["sender"]}</span>
+                  <br />
+                  <div className={"msg-text"}>{message["msg"]}</div>
+                </div>
+              );
+          })}
+          <div ref={messagesEndRef} />
         </div>
-      ) : (
-        <div className="chat">
-          <div
-            className="messages"
-            ref={messagesDivRef}
-            onWheel={(e) => {
-              console.log(scrolledToBottom(messagesDivRef.current));
-              if (scrolledToBottom(messagesDivRef.current)) {
-                setScrolled(false);
-              } else {
-                setScrolled(true);
-              }
+        {scrolled ? (
+          <Down
+            id="scroll-icon"
+            onClick={(e) => {
+              messagesEndRef.current.scrollIntoView();
+              setScrolled(false);
             }}
-            onTouchMove={(e) => {
-              // if (elementInViewport(messagesEndRef.current)) {
-              //   setScrolled(false);
-              // } else {
-              //   setScrolled(true);
-              // }
-            }}
-          >
-            {messages.map((message) => {
-              // console.log(message);
-              if (message["sender"] === nickname)
-                return (
-                  <div className="my-message">
-                    {/*<span id="me">{message["sender"]} :</span>*/}
-                    {/*<br />*/}
-                    <div className={"msg-text"}>{message["msg"]}</div>
-                  </div>
-                );
-              else
-                return (
-                  <div className="message">
-                    <span id="sender">{message["sender"]}</span>
-                    <br />
-                    <div className={"msg-text"}>{message["msg"]}</div>
-                  </div>
-                );
-            })}
-            <div ref={messagesEndRef} />
-          </div>
-          {/*{scrolled ? (
-            <button
-              id="scrollbutton"
-              onClick={(e) => {
-                console.log("bnmbnm");
-                messagesEndRef.current.scrollIntoView();
-                setScrolled(false);
-              }}
-            >
-              Scroll to bottom
-            </button>
-          ) : (
-            ""
-          )}*/}
-          {scrolled ? (
-            <Down
-              id="scroll-icon"
-              onClick={(e) => {
-                console.log("bnmbnm");
-                messagesEndRef.current.scrollIntoView();
-                setScrolled(false);
-              }}
-            />
-          ) : (
-            ""
-          )}
-          <div className={"chat-input"}>
-            <input
-              placeholder="msg"
-              onChange={handleMsgChange}
-              onKeyDown={handleKeyDown}
-              value={msg}
-            />
-          </div>
+          />
+        ) : (
+          ""
+        )}
+        <div className={"chat-input"}>
+          <input
+            placeholder="msg"
+            onChange={handleMsgChange}
+            onKeyDown={handleKeyDown}
+            value={msg}
+          />
         </div>
-      )}
+      </div>
     </div>
   );
 };
