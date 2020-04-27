a3265bc0473b4866b5b01aec43aba4cf34ed56c9
AFNAN NAVAZ <afnan@pop-os.localdomain>
2020-04-27 18:41:38 +0530

    new character

--- src/components/doorboard/Doorboard.jsx
+++ src/components/doorboard/Doorboard.jsx
@@ -1,10 +1,20 @@
 import './Doorboard.scss';
-import React from 'react';
+import React, {useEffect} from 'react';
 import Doorinfo from './doorinfo/Doorinfo';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faAngleRight} from '@fortawesome/free-solid-svg-icons';
+import {API_ROOT} from '../../auth0/api_config';
+import {get} from '../../auth0/http';
 
 function Doorboard(props) {
+
+    useEffect(()=>{
+        (async () => {
+            let res = await get(`${API_ROOT}leaderboard`);
+            console.log(res);
+        })();
+    }, []);
+
     return(
         <div id="door-board" className={`bor cursor-default ${props.bor ? "bor-tra" : ""}`}>
             <div className={`toggle-2 cursor-pointer ${props.bor ? "togg-tra" : ""}`} onClick={() => props.toggle()}>
