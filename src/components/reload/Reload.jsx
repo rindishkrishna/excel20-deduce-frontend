a6661920ee7f451fcd6530402e4a76928aac1a70
ameen4455 <ameen4455@gmail.com>
2020-05-03 16:30:05 +0530

    add landing page

--- src/components/reload/Reload.jsx
+++ src/components/reload/Reload.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Reload.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faRedoAlt} from '@fortawesome/free-solid-svg-icons';
 
 
 function Reload(props) {
+    const [fullscreen,setFullScreen] = useState(true);
+
+    useEffect(() =>{
+            if(!window.fullscreen && window.innerWidth < 961     ){
+                setFullScreen(false)
+            }
+            else {
+            setFullScreen(true)
+        }
+    },[]);
+
+
     return(
-        <div className="reload text-center">
-            <h1>Rotate your device</h1>
-            <FontAwesomeIcon className="r-btn" icon={faRedoAlt} onClick={() => {
-                if(window.innerHeight < window.innerWidth){
-                    let x = document.documentElement.requestFullscreen()
-                    x.catch(()=>alert("no fullscreen"))
-                    props.screen(true);
-                }
-            }} />
-            <p>click</p>
+        <>
+        <div className={"rotate reload text-center"}>
+            Please Rotate Your Screen
         </div>
+            {!fullscreen ?
+                <div className="reload text-center" onClick={() => {
+                    if (window.innerHeight < window.innerWidth) {
+                        setFullScreen(true);
+                        let x = document.documentElement.requestFullscreen();
+                        x.catch(() => alert("no fullscreen"));
+                        props.setScreen(true);
+                    }
+                }}>
+                    Touch To Go Fullscreen
+                </div>
+                :
+                null
+            }
+        </>
     );
 }
 
